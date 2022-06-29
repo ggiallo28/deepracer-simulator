9654a7a26f60b3d9ecaf42e43aaabe28748f4bed
Gianluigi Mucciolo <gianluigi.mucciolo@it.clara.net>
2022-06-29 18:41:31 +0200

    add track renderer

--- gym_multi_deepracer/multi_deepracer.py
+++ gym_multi_deepracer/multi_deepracer.py
@@ -1,23 +1,17 @@
-import sys
-sys.path.append(".")
-
-import math
-import numpy as np
-
-import Box2D
-
-import gym
+from calendar import c
+import Box2D, gym
 import gym.envs.box2d.car_dynamics as car_dynamics
-from gym import spaces
 from gym.utils import seeding, EzPickle
+from gym import spaces
+import numpy as np
+import math
 
-import pyglet
-from pyglet import gl
-from shapely.geometry import Point, Polygon
+from shapely.geometry import Point
 
-from friction_detector import FrictionDetector
-from deepracer_parameters import DeepRacerParameters
-from track_builder import TrackBuilder
+from src.friction_detector import FrictionDetector
+from src.deepracer_parameters import DeepRacerParameters
+from src.track_builder import TrackBuilder
+from src.gym_renderer import GymRenderer
 
 # Easiest continuous control task to learn from pixels, a top-down racing environment.
 # Discrete control is reasonable in this environment as well, on/off discretization is
@@ -155,6 +149,8 @@ class MultiDeepRacer(gym.Env, EzPickle):
             low=0, high=255, shape=(STATE_H, STATE_W, 3)
         )
 
+        self.renderer = GymRenderer(self)
+
     def seed(self, seed=None):
         self.np_random, seed = seeding.np_random(seed)
         return [seed]
@@ -170,13 +166,54 @@ class MultiDeepRacer(gym.Env, EzPickle):
             car.destroy()
 
     def _create_track(self):
-        (
-            self.track,
-            self.road_poly,
-            self.road,
-            self.road_poly_shapely,
-        ) = TrackBuilder(self.np_random, ROAD_COLOR, self.world, self.num_agents).get()
+        (self.track, self.road_poly, self.road, self.road_poly_shapely,) = TrackBuilder(
+            self.np_random, ROAD_COLOR, self.world, self.num_agents
+        ).get()
         return True
+
+    def reset_car(self, car_id):
+        (angle, pos_x, pos_y) = self.track[0][1:4]
+
+        # Specify line and lateral separation between cars
+        line_spacing = LINE_SPACING
+        lateral_spacing = LATERAL_SPACING
+
+        # index into positions using modulo and pairs
+        line_number = math.floor(self.car_order[car_id] / 2)  # Starts at 0
+        side = (2 * (self.car_order[car_id] % 2)) - 1  # either {-1, 1}
+
+        # Compute offsets from start (this should be zero for first pair of cars)
+        dx = self.track[-line_number * line_spacing][2] - pos_x  # x offset
+        dy = self.track[-line_number * line_spacing][3] - pos_y  # y offset
+
+        # Compute angle based off of track index for car
+        angle = self.track[-line_number * line_spacing][1]
+        if self.episode_direction == "CW":  # CW direction indicates reversed
+            angle -= np.pi  # Flip direction is either 0 or pi
+
+        # Compute offset angle (normal to angle of track)
+        norm_theta = angle - np.pi / 2
+
+        # Compute offsets from position of original starting line
+        new_x = pos_x + dx + (lateral_spacing * np.sin(norm_theta) * side)
+        new_y = pos_y + dy + (lateral_spacing * np.cos(norm_theta) * side)
+
+        # Display spawn locations of cars.
+        # print(f"Spawning car {car_id} at {new_x:.0f}x{new_y:.0f} with "
+        #       f"orientation {angle}")
+
+        # Create car at location with given angle
+        self.cars[car_id] = car_dynamics.Car(self.world, angle, new_x, new_y)
+        self.cars[car_id].hull.color = CAR_COLORS[car_id % len(CAR_COLORS)]
+
+        # This will be used to identify the car that touches a particular tile.
+        for wheel in self.cars[car_id].wheels:
+            wheel.car_id = car_id
+
+        print(self.reward, self.prev_reward)
+        self.reward[car_id] = 0.0
+        self.prev_reward[car_id] = 0.0
+        print(self.reward, self.prev_reward)
 
     def reset(self):
         self._destroy()
@@ -198,58 +235,16 @@ class MultiDeepRacer(gym.Env, EzPickle):
         shuffle_ids = np.random.choice(ids, size=self.num_agents, replace=False)
         self.car_order = {i: shuffle_ids[i] for i in range(self.num_agents)}
 
-        while True:
+        success = False
+        while not success:
             success = self._create_track()
-            if success:
-                break
             if self.verbose == 1:
                 print(
                     "retry to generate track (normal if there are not many of this messages)"
                 )
 
-        (angle, pos_x, pos_y) = self.track[0][1:4]
-        car_width = car_dynamics.SIZE * (
-            car_dynamics.WHEEL_W * 2
-            + (car_dynamics.WHEELPOS[1][0] - car_dynamics.WHEELPOS[1][0])
-        )
         for car_id in range(self.num_agents):
-
-            # Specify line and lateral separation between cars
-            line_spacing = LINE_SPACING
-            lateral_spacing = LATERAL_SPACING
-
-            # index into positions using modulo and pairs
-            line_number = math.floor(self.car_order[car_id] / 2)  # Starts at 0
-            side = (2 * (self.car_order[car_id] % 2)) - 1  # either {-1, 1}
-
-            # Compute offsets from start (this should be zero for first pair of cars)
-            dx = self.track[-line_number * line_spacing][2] - pos_x  # x offset
-            dy = self.track[-line_number * line_spacing][3] - pos_y  # y offset
-
-            # Compute angle based off of track index for car
-            angle = self.track[-line_number * line_spacing][1]
-            if self.episode_direction == "CW":  # CW direction indicates reversed
-                angle -= np.pi  # Flip direction is either 0 or pi
-
-            # Compute offset angle (normal to angle of track)
-            norm_theta = angle - np.pi / 2
-
-            # Compute offsets from position of original starting line
-            new_x = pos_x + dx + (lateral_spacing * np.sin(norm_theta) * side)
-            new_y = pos_y + dy + (lateral_spacing * np.cos(norm_theta) * side)
-
-            # Display spawn locations of cars.
-            # print(f"Spawning car {car_id} at {new_x:.0f}x{new_y:.0f} with "
-            #       f"orientation {angle}")
-
-            # Create car at location with given angle
-            self.cars[car_id] = car_dynamics.Car(self.world, angle, new_x, new_y)
-            self.cars[car_id].hull.color = CAR_COLORS[car_id % len(CAR_COLORS)]
-
-            # This will be used to identify the car that touches a particular tile.
-            for wheel in self.cars[car_id].wheels:
-                wheel.car_id = car_id
-
+            self.reset_car(car_id=car_id)
         return self.step(None)[0]
 
     def step(self, action):
@@ -275,8 +270,8 @@ class MultiDeepRacer(gym.Env, EzPickle):
         self.steps += 1.0
 
         self.state = self.render("state_pixels")
-
         step_reward = np.zeros(self.num_agents)
+
         done = False
         if action is not None:  # First step without action, called from reset()
             self.reward -= 0.1
@@ -355,197 +350,21 @@ class MultiDeepRacer(gym.Env, EzPickle):
                     done = True
                     step_reward[car_id] = -100
 
-        # step_reward = [0 if r < 0 else 1 for r in step_reward]
+        for car_id, car in enumerate(self.cars):
+            if self.driving_on_grass[car_id]:
+                self.reset_car(car_id=car_id)
+                step_reward[car_id] = 0.0
+
+        step_reward = step_reward.clip(min=0.0)
         return self.state, step_reward, done, {}
 
     def render(self, mode="human", close=False):
         assert mode in ["human", "state_pixels", "rgb_array"]
 
+        self.renderer.update()
+
         result = []
         for cur_car_id in range(self.num_agents):
-            result.append(self._render_window(cur_car_id, mode))
+            result.append(self.renderer.render_window(cur_car_id, mode))
 
         return np.stack(result, axis=0)
-
-    def _render_window(self, car_id, mode):
-        """ Performs the actual rendering for each car individually. 
-        
-        Parameters:
-            car_id(int): Numerical id of car for which the corresponding window
-                will be rendered.
-            mode(str): Rendering mode.
-        """
-
-        if self.viewer[car_id] is None:
-            from gym.envs.classic_control import rendering
-
-            self.viewer[car_id] = rendering.Viewer(WINDOW_W, WINDOW_H)
-            self.viewer[car_id].window.set_caption(f"Car {car_id}")
-            self.score_label = pyglet.text.Label(
-                "0000",
-                font_size=36,
-                x=20,
-                y=WINDOW_H * 2.5 / 40.00,
-                anchor_x="left",
-                anchor_y="center",
-                color=(255, 255, 255, 255),
-            )
-            self.transform = rendering.Transform()
-
-        if "t" not in self.__dict__:
-            return  # reset() not called yet
-
-        zoom = 0.1 * TRACK_SCALE * max(1 - self.t, 0) + ZOOM * TRACK_SCALE * min(
-            self.t, 1
-        )  # Animate zoom first second
-        # NOTE (ig): Following two variables seemed unused. Commented them out.
-        # zoom_state  = ZOOM*TRACK_SCALE*STATE_W/WINDOW_W
-        # zoom_video  = ZOOM*TRACK_SCALE*VIDEO_W/WINDOW_W
-        scroll_x = self.cars[car_id].hull.position[0]
-        scroll_y = self.cars[car_id].hull.position[1]
-        angle = -self.cars[car_id].hull.angle
-        vel = self.cars[car_id].hull.linearVelocity
-        if np.linalg.norm(vel) > 0.5:
-            angle = math.atan2(vel[0], vel[1])
-        self.transform.set_scale(zoom, zoom)
-
-        # Positions car in the center with regard to the window width and 1/4 height away from the bottom.
-        self.transform.set_translation(
-            WINDOW_W / 2
-            - (scroll_x * zoom * math.cos(angle) - scroll_y * zoom * math.sin(angle)),
-            WINDOW_H * self.h_ratio
-            - (scroll_x * zoom * math.sin(angle) + scroll_y * zoom * math.cos(angle)),
-        )
-        self.transform.set_rotation(angle)
-
-        # Set colors for each viewer and draw cars
-        for id, car in enumerate(self.cars):
-            if self.use_ego_color:  # Apply same ego car color coloring scheme
-                car.hull.color = (0.0, 0.0, 0.8)  # Set all other car colors to blue
-                if id == car_id:  # Ego car
-                    car.hull.color = (0.8, 0.0, 0.0)  # Set ego car color to red
-            car.draw(self.viewer[car_id], mode != "state_pixels")
-
-        arr = None
-        win = self.viewer[car_id].window
-        win.switch_to()
-        win.dispatch_events()
-
-        win.clear()
-        t = self.transform
-        if mode == "rgb_array":
-            VP_W = VIDEO_W
-            VP_H = VIDEO_H
-        elif mode == "state_pixels":
-            VP_W = STATE_W
-            VP_H = STATE_H
-        else:
-            pixel_scale = 1
-            if hasattr(win.context, "_nscontext"):
-                pixel_scale = (
-                    win.context._nscontext.view().backingScaleFactor()
-                )  # pylint: disable=protected-access
-            VP_W = int(pixel_scale * WINDOW_W)
-            VP_H = int(pixel_scale * WINDOW_H)
-
-        gl.glViewport(0, 0, VP_W, VP_H)
-        t.enable()
-        self.render_road()
-        for geom in self.viewer[car_id].onetime_geoms:
-            geom.render()
-        self.viewer[car_id].onetime_geoms = []
-        t.disable()
-        self.render_indicators(car_id, WINDOW_W, WINDOW_H)
-
-        if mode == "human":
-            win.flip()
-            return not self.viewer[car_id].window_closed_by_user
-
-        image_data = (
-            pyglet.image.get_buffer_manager().get_color_buffer().get_image_data()
-        )
-        # np.frombuffer(astr.encode(),'u1')
-        # arr = np.fromstring(image_data.get_data(), dtype=np.uint8, sep="")
-        arr = np.frombuffer(image_data.get_data(), "u1")
-        arr = arr.reshape(VP_H, VP_W, 4)
-        arr = arr[::-1, :, 0:3]
-
-        return arr
-
-    def close(self):
-        if None not in self.viewer:
-            for viewer in self.viewer:
-                viewer.close()
-
-        self.viewer = [None] * self.num_agents
-
-    def render_road(self):
-        gl.glBegin(gl.GL_QUADS)
-        gl.glColor4f(0.4, 0.8, 0.4, 1.0)
-        gl.glVertex3f(-PLAYFIELD, +PLAYFIELD, 0)
-        gl.glVertex3f(+PLAYFIELD, +PLAYFIELD, 0)
-        gl.glVertex3f(+PLAYFIELD, -PLAYFIELD, 0)
-        gl.glVertex3f(-PLAYFIELD, -PLAYFIELD, 0)
-        gl.glColor4f(0.4, 0.9, 0.4, 1.0)
-        k = PLAYFIELD / 20.0
-        for x in range(-20, 20, 2):
-            for y in range(-20, 20, 2):
-                gl.glVertex3f(k * x + k, k * y + 0, 0)
-                gl.glVertex3f(k * x + 0, k * y + 0, 0)
-                gl.glVertex3f(k * x + 0, k * y + k, 0)
-                gl.glVertex3f(k * x + k, k * y + k, 0)
-        for poly, color in self.road_poly:
-            gl.glColor4f(color[0], color[1], color[2], 1)
-            for p in poly:
-                gl.glVertex3f(p[0], p[1], 0)
-        gl.glEnd()
-
-    def render_indicators(self, agent_id, W, H):
-        gl.glBegin(gl.GL_QUADS)
-        s = W / 40.0
-        h = H / 40.0
-        gl.glColor4f(0, 0, 0, 1)
-        gl.glVertex3f(W, 0, 0)
-        gl.glVertex3f(W, 5 * h, 0)
-        gl.glVertex3f(0, 5 * h, 0)
-        gl.glVertex3f(0, 0, 0)
-
-        def vertical_ind(place, val, color):
-            gl.glColor4f(color[0], color[1], color[2], 1)
-            gl.glVertex3f((place + 0) * s, h + h * val, 0)
-            gl.glVertex3f((place + 1) * s, h + h * val, 0)
-            gl.glVertex3f((place + 1) * s, h, 0)
-            gl.glVertex3f((place + 0) * s, h, 0)
-
-        def horiz_ind(place, val, color):
-            gl.glColor4f(color[0], color[1], color[2], 1)
-            gl.glVertex3f((place + 0) * s, 4 * h, 0)
-            gl.glVertex3f((place + val) * s, 4 * h, 0)
-            gl.glVertex3f((place + val) * s, 2 * h, 0)
-            gl.glVertex3f((place + 0) * s, 2 * h, 0)
-
-        true_speed = np.sqrt(
-            np.square(self.cars[agent_id].hull.linearVelocity[0])
-            + np.square(self.cars[agent_id].hull.linearVelocity[1])
-        )
-        vertical_ind(5, 0.02 * true_speed, (1, 1, 1))
-        vertical_ind(
-            7, 0.01 * self.cars[agent_id].wheels[0].omega, (0.0, 0, 1)
-        )  # ABS sensors
-        vertical_ind(8, 0.01 * self.cars[agent_id].wheels[1].omega, (0.0, 0, 1))
-        vertical_ind(9, 0.01 * self.cars[agent_id].wheels[2].omega, (0.2, 0, 1))
-        vertical_ind(10, 0.01 * self.cars[agent_id].wheels[3].omega, (0.2, 0, 1))
-        horiz_ind(20, -10.0 * self.cars[agent_id].wheels[0].joint.angle, (0, 1, 0))
-        horiz_ind(30, -0.8 * self.cars[agent_id].hull.angularVelocity, (1, 0, 0))
-        gl.glEnd()
-        self.score_label.text = "%04i" % self.reward[agent_id]
-        self.score_label.draw()
-
-        # Render backwards flag if driving backward and backwards flag render is enabled
-        if self.driving_backward[agent_id] and self.backwards_flag:
-            pyglet.graphics.draw(
-                3,
-                gl.GL_TRIANGLES,
-                ("v2i", (W - 100, 30, W - 75, 70, W - 50, 30)),
-                ("c3B", (0, 0, 255) * 3),
-            )
